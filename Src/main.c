e7769671c4273fc069f15f06d8eb8b88ec25dd02
JonathanPLev <jplevitsky@ucdavis.edu>
2024-10-24 09:40:47 -0700

    modified:   Src/main.c

--- Src/main.c
+++ Src/main.c
@@ -7,5 +7,31 @@
 int main(void)
 {
     /* Loop forever */
-	for(;;);
+	while (1) {
+
+			if (get_buttonStatus_1()) {
+				// Do whatever you’d want to do if Button 1 was pressed
+				printf("Button 1 Pressed");
+			}
+			if (get_buttonStatus_0()) {
+				// Do whatever you’d want to do if Button 1 was pressed
+				printf("Button 0 Pressed");
+			}
+
+			int current_time = getSysTime();
+			while (getSysTime() - current_time < 1000){
+				continue;
+			}
+			// led_a(true);	// This turns on the LED marked as LED-A on the Orbital Platform
+			current_time = getSysTime();
+			while (getSysTime() - current_time < 500){
+						continue;
+					}
+			// led_a(false);	// This turns off LED-A
+
+			_gpio_high(GPIOD, pin);
+			delay_ms(10);
+			_gpio_low(GPIOD, pin);
+
+		};
 }
