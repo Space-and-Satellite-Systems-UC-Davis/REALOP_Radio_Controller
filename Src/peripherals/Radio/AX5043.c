91495bb8a2568d0d7beb2a6d4ed2f7c8affc08ad
Ethan See <ethan_see@hotmail.com>
2025-04-23 20:49:32 -0700

    untested implementation of autoranging

--- Src/peripherals/Radio/AX5043.c
+++ Src/peripherals/Radio/AX5043.c
@@ -6,48 +6,63 @@ void radio_init() {
     uhf_init();
 }
 
-
-void uhf_init() {
-    gpio_high(GPIOB, 9); // Enable power to UHF Transceiver
-    return;
-
-    //Setting the reset modes
-    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_RST); // Turn on reset bit
-    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_POWERDOWN); // Set it to power down (only register file stays on) while also clearing reset bit
-
-    // Setting parameters
-    
-    // Auto ranging
-    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_STANDBY);
-    radio_autorange(AX5043_CARRIER_HZ, AX5043_XTAL_HZ);
-    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_POWERDOWN);
+void programParametersFromRadioLab() {
 
 }
 
-int radio_autorange(int carrierHz, int xtalHz) {
+void uhf_init() {
+    gpio_high(GPIOB, 9); // Enable power to UHF Transceiver
+    
+    spi_startCommunication(UHF_SPI_CS);
+
+    while(!gpio_read(GPIOB, 14)); //Wait for MISO to go HIGH 
+
+    uint8_t initialValueOfPWRMODE = ax5043_read8(AX5043_PWRMODE);
+
+    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_RST | initialValueOfPWRMODE); //Turn on RST bit
+    nop(50);
+    ax5043_write8(AX5043_PWRMODE, !AX5043_PWRMODE_RST | initialValueOfPWRMODE); //Turn off RST bit
+
+    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_POWERDOWN);
+
+    uhf_programParametersFromRadioLab(); //TODO: Get those parameters
+
+    int failCount = 0;
+    while(!radio_autorange(AX5043_CARRIER_HZ, AX5043_XTAL_HZ)) {
+        if (failCount > 3) {
+            break;
+        }
+        failCount++;
+    }
+
+}
+
+bool radio_autorange(int carrierHz, int xtalHz) {
 	int freqa = ((uint_fast64_t)(carrierHz)<<24)/xtalHz | 1;
 	ax5043_write8(AX5043_FREQA0, (freqa >> 0) & 0xFF);
 	ax5043_write8(AX5043_FREQA1, (freqa >> 8) & 0xFF);
 	ax5043_write8(AX5043_FREQA2, (freqa >> 16) & 0xFF);
 	ax5043_write8(AX5043_FREQA3, (freqa >> 24) & 0xFF);
 	
-	ax5043_write8(AX5043_PLLRANGINGA, 0x18); // start ranging 
-	
-	int count = 0;
-	while (!(ax5043_read8(AX5043_IRQREQUEST1) & 0x10)) {
-		count++;
-		if (count > 0xFFFF) {
-			break;
-		}
-	}
+    ax5043_write8(AX5043_PLLRANGINGA, AX5043_PLLRANGINGA_VCORA);
 
-	if (ax5043_read8(AX5043_PLLRANGINGA) & (0x20|0x10)) { // check for "ranging error" and "ranging active"
-		return 1;
-	} else {
-		// dmsg("Synthesizer autorange OK");
-		return 0;
-	}
+    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_STANDBY | 0b11 << 5); //Not sure if i need to do the shifting but the normal reset value has REFEN and XOEN on
+    while(!ax5043_read8(AX5043_XTALSTATUS)); //Wait for crystal to be ready 
+    
+	ax5043_write8(AX5043_PLLRANGINGA, ax5043_read8(AX5043_PLLRANGINGA) | AX5043_PLLRANGINGA_RNGSTART); // start ranging 
+	
+    while(!(ax5043_read8(AX5043_PLLRANGINGA) & (AX5043_PLLRANGINGA_RNGSTART))); //Wait for RNGSTART to read 0 indicating it is done
+
+    ax5043_write8(AX5043_PWRMODE, AX5043_PWRMODE_POWERDOWN | 0b11 << 5);
+
+    if (ax5043_read8(AX5043_PLLRANGINGA) & AX5043_PLLRANGINGA_RNGERR) {
+        return false; //Indicates an error in auto ranging, try again
+    }
+
+    return true;
 }
+
+
 
 uint8_t ax5043_read8(uint32_t address) {
     uint8_t addr[2];
