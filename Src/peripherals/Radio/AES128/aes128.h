dbd5e9ca0079f9edbcd5588826e1542f03c3cd67
Tanishq Dwivedi <tanishq12.dwivedi@gmail.com>
2025-10-21 14:33:18 -0700

    Need Review and Documentation of Newly Created Functions.

---
+++ Src/peripherals/Radio/AES128/aes128.h
@@ -0,0 +1,10 @@
+/*
+ * aes128.h
+ *
+ * 	- October 15, 2025
+ * 		Author : Tanishq
+ * 		Log    : AES128 Encryption Task
+ */                                                                            \
+#ifndef ENCRYPTION_AES123_H_
+#define ENCRYPTION_AES123_H_
+
