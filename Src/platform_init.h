a7f25e8de959cd725b5c78db33eb05ed8cd70678
HistidineDwarf <dvorakdwarf@gmail.com>
2026-04-01 18:54:12 -0700

    Update platform_init.h

--- Src/platform_init.h
+++ Src/platform_init.h
@@ -33,19 +33,13 @@ void init_platform() {
 	
 	SCB->CPACR |= 0x00F00000;
 	init_coreClocks();
-
-
+	
 	gpio_init();
 
-
-	// init intercomm
-	// init radio
 	led_init();
 	printer_init();
 	systick_init();
-	// RTC init
 	rtc_config(LSI, 0);
-	// radio init
 	radio_init();
 }
 
