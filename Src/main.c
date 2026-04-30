301d62aa8374ed67bd1c02c0a9c1d8141ac59aec
UmbraGlobe <officialumbragames@gmail.com>
2026-04-30 16:44:07 -0700

    added more functionality to dma

--- Src/main.c
+++ Src/main.c
@@ -11,28 +11,9 @@ bool test_radio_reads_simple();
 
 int main(void)
 {
-    /* Loop forever */
     init_platform();
-	
-	#if (RUN_TEST==1) && (TEST_ID != 0)
-
-    void (*testFunc)();
-    testFunc = getTestFunction(TEST_ID);
-    testFunc();
-
-    #else
-
-	//TODO: use RTC first_time flag.
-	//if (first_time) {
-	//  init_first_time()
-	//}
-
-	while (1) {
-		continue;
-	}
-
-#endif
-	
+	dma_init();
+	testFunction_UART();
 }
 
 bool test_radio_reads_simple() {
