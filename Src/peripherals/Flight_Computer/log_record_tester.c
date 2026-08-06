f4722dc5130341ae41b94da08817016cb02fe429
HistidineDwarf <dvorakdwarf@gmail.com>
2026-08-05 21:45:44 -0700

    Instructions for testing in lieu of printing

--- Src/peripherals/Flight_Computer/log_record_tester.c
+++ Src/peripherals/Flight_Computer/log_record_tester.c
@@ -14,6 +14,16 @@
 
 #define PFC_USART USART1
 
+// INSTRUCTIONS FOR TESTING (assuming CubeIDE debugger):
+// 1. Flash the PFC with the test of the same name and ID
+// 2. Use the debugger to check the first few fields of the log
+//	  in radio_downlink_idle_log(). boot_counter and year/month
+// 3. Set breakpoint on the end of crc_chunked_read() on Radio
+// 4. Continue until it breaks there.
+// 5. Open Intercomm.c and hover over "storedData"
+// 6. That should show the contents. Check if bytes match what it was on PFC.
+//    Should be some boot count number and then 0, 1, 1. Check step 2.
+// 7. If you have the ability to read radio, check it's sending it correctly
 void testFunction_LogDownlink() {
     usart_init(PFC_USART, 9600);
 
