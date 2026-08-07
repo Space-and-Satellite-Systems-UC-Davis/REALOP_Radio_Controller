acb53fa5fad9a8d9decbd6558b3197111dd34c47
James Knepper <jwknepper@ucdavis.edu>
2026-08-06 19:25:53 -0700

    Add tests to testing suite, update calls to reflect new calling convention

--- Src/peripherals/Radio/EncFECAuth.c
+++ Src/peripherals/Radio/EncFECAuth.c
@@ -11,7 +11,8 @@ size_t FAEEncodeBlock(char *input, char *output, uint64_t index, size_t block_si
     size_t length = block_size;
     Encrypt_Array(workpad, &length);
     // TODO if needed: separate authentication (e.g. append a shared private key to result in a workpad and hash, then appent hash to message)
-    ReedSolomon rs = RSGF_construct_ReedSolomon(length, FAE_ECC_SIZE);
+    ReedSolomon rs;
+    RSGF_construct_ReedSolomon(&rs, length, FAE_ECC_SIZE);
     length += rs.ecc_length;
     RSGF_encode_message(&rs, workpad, output + index*length);
     return length;
@@ -43,7 +44,8 @@ FAEBlockData FAEEncodeStream(char *in, char *out, size_t nbytes, size_t output_b
 size_t FAEDecodeBlock(char *input, char *output, uint64_t index, size_t block_size, char *workpad) {
     size_t length;
     length = block_size;
-    ReedSolomon rs = RSGF_construct_ReedSolomon(length - FAE_ECC_SIZE, FAE_ECC_SIZE);
+    ReedSolomon rs;
+    RSGF_construct_ReedSolomon(&rs, length - FAE_ECC_SIZE, FAE_ECC_SIZE);
     RSGF_decode_message(&rs, output + index*length, workpad, NULL, 0);
     length -= FAE_ECC_SIZE;
     // TODO if needed: check authentication (e.g. append a shared private key to contents in workpad and hash, check that hash matches)
--- Src/peripherals/Radio/testEncFECAuth.c
+++ Src/peripherals/Radio/testEncFECAuth.c
@@ -1,10 +1,18 @@
 #include "EncFECAuth.h"
+#include "Reed-Solomon/rs.h"
 #include "print_scan.h"
 
 void testFunction_FAE() {
     char nonce[] = "HELLO TOBIAS I MAKE THE GLOBULINS";
     char enc[256] = "\0";
     char dec[256] = "\0";
+    ReedSolomon rs;
+    RSGF_construct_ReedSolomon(&rs, 224, 16);
+    while (true) {
+        RSGF_encode_message(&rs, nonce, enc);
+        RSGF_decode_message(&rs, enc, dec, NULL, 0);
+        if (strcmp(dec, nonce) == 0) break;
+    }
     while (true) {
         FAEBlockData bd = FAEEncodeStream(nonce, &enc[0], sizeof nonce, 249);
         bd = FAEDecodeStream(&enc[0], &dec[0], bd.nbytes, bd.block_size);
